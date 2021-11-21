9ac63feb68ee0f25b79fb778f390d2f32b43fefb
sumin123 <soomin61@snu.ac.kr>
2021-11-21 16:48:49 +0900

    [feat] 로그인 세션 구현 중

--- src/components/sign-up-google/index.js
+++ src/components/sign-up-google/index.js
@@ -6,15 +6,29 @@ function SignUpGooglePage() {
   useEffect(() => {
     alert("회원가입이 필요합니다.")
   }, []);
-  
+
+  const send = () => {
+    const nickname = document.querySelector('#head').value;
+    console.log(nickname);
+    fetch(`${process.env.REACT_APP_SERVER_IP}/google-sign-in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nickname: nickname,
+      })
+    })
+  }
+
   return (
     <div className="sign-up-write-google">
       <CommonHeader />
       <div className="input-container">
         닉네임
-        <input className="head" id="head" name="title" type="text" placeholder="제목" />
+        <input className="head" id="head" name="nickname" type="text" placeholder="닉네임" />
       </div>
-      <button type="submit" className="submit-btn-google">
+      <button type="submit" className="submit-btn-google" onClick={send}>
         회원가입
       </button>
     </div>
