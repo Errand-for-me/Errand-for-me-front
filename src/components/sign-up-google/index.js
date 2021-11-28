5188f791da0f021f9931f0d1c6517cf13989402e
sumin123456 <soomin5234@gmail.com>
2021-11-28 10:42:56 +0900

    [feat] 닉네임 유효성 검사 추가

--- src/components/sign-up-google/index.js
+++ src/components/sign-up-google/index.js
@@ -18,28 +18,36 @@ function SignUpGooglePage() {
   }, []);
 
   const send = async () => {
-    const nickname = document.querySelector("#head").value;
-    const result = await fetch(`${process.env.REACT_APP_SERVER_IP}/google-sign-in`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        nickname: nickname,
-      }),
-      credentials: "include",
-    });
-
-    const data = await result.text();
-    if (data === "true") {
-      setLoginInfo({ isLogin: true, nickname: nickname });
-      history.goBack();
-    } else {
+    const nickname = document.querySelector("#head").value.trim();
+    if (nickname.length < 2 || nickname.length > 15) {
       document.body.style.overflow = "hidden";
       const modal = document.querySelector("#modal");
-      modal.querySelector(".modal-title").innerHTML = "회원 가입 오류!";
-      modal.querySelector(".modal-content").innerHTML = "닉네임이 중복된 것 같아요!";
+      modal.querySelector(".modal-title").innerHTML = "닉네임 검사";
+      modal.querySelector(".modal-content").innerHTML = "3자에서 15자 사이로 지정해주세요!";
       modal.style.display = "flex";
+    } else {
+      const result = await fetch(`${process.env.REACT_APP_SERVER_IP}/google-sign-in`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nickname: nickname,
+        }),
+        credentials: "include",
+      });
+
+      const data = await result.text();
+      if (data === "true") {
+        setLoginInfo({ isLogin: true, nickname: nickname });
+        history.goBack();
+      } else {
+        document.body.style.overflow = "hidden";
+        const modal = document.querySelector("#modal");
+        modal.querySelector(".modal-title").innerHTML = "회원 가입 오류!";
+        modal.querySelector(".modal-content").innerHTML = "닉네임이 중복된 것 같아요!";
+        modal.style.display = "flex";
+      }
     }
   };
 
